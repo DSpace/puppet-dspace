e43e81c011347f40dd61717178d657f0640a3167
Tim Donohue <tdonohue@duraspace.org>
2016-08-31 17:37:23 +0000

    Minor bug fixes. Add basic SSL support

--- manifests/apache_site.pp
+++ manifests/apache_site.pp
@@ -16,39 +16,45 @@
 # - Ubuntu 16.04
 #
 # Parameters:
-# - $package        => Apache package to install/use (e.g. 'apache2')
-# - $owner          => OS account which should own Tomcat (i.e. who Tomcat should run as). Default=name of package (e.g. tomcat8)
-# - $group          => OS group which should own Tomcat (default is same as $owner)
-# - $service        => Name of the default Tomcat service (default is same as $package)
-# - $catalina_home  => Full path to Catalina Home (default=/usr/share/$package), i.e. installation directory
-# - $catalina_base  => Full path to Catalina Base (default=/var/lib/$package), i.e. instance directory
-# - $app_base       => Directory where Tomcat instance should load webapps (default=$name)
-# - $port           => Port this Tomcat instance runs on
-# - $catalina_opts  => Options to pass to Tomcat (default='-Djava.awt.headless=true -Dfile.encoding=UTF-8 -Xmx2048m -Xms1024m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC')
-# - $ensure         => Whether to install (ensure=present) or remove (ensure=absent) this Tomcat instance (default=present)
+# - $hostname        => Hostname for this Apache site
+# - $tomcat_ajp_port => Tomcat AJP port to forward requests to
+# - $ssl             => Whether to enable SSL for this site
+# - $ssl_cert        => Optional SSL cert path if SSL enabled
+# - $ssl_chain       => Optional SSL chain path if SSL enabled
+# - $ssl_key         => Optional SSL key path if SSL enabled
+# - $ensure          => Whether to install (ensure=present) or remove (ensure=absent) this Apache site (default=present)
 #
 # Sample Usage:
-# dspace::apache_site {
+# dspace::apache_site { 'dspace.myu.edu':
 #    tomcat_ajp_port    => 8009,
 # }
 define dspace::apache_site ($hostname        = $name,
                             $tomcat_ajp_port = $dspace::tomcat_ajp_port,
+                            $ssl             = false,
+                            $ssl_cert        = undef,
+                            $ssl_chain       = undef,
+                            $ssl_key         = undef,
                             $ensure          = present)
 {
 
   case $ensure
   {
-    # Present = Install/Setup Tomcat
+    # Present = Install/Setup Apache
     present: {
 
-      # Install Apache. Turn off default vhost.
+      # Install Apache. Turn off default vhost (we want DSpace to be default)
       class { 'apache':
         default_vhost => false,
       }
 
+      # Install mod_proxy and mod_proxy_ajp
+      # These modules are needed to proxy all requests to Tomcat
+      class { 'apache::mod::proxy': }
+      class { 'apache::mod::proxy_ajp': }
+
       # Create a virtual host for our site, running on port 80
       # and proxying ALL requests to Tomcat's AJP port.
-      apache::vhost { "${hostname} non-ssl":
+      apache::vhost { $hostname:
         servername => $hostname,
         port       => 80,
         proxy_pass => [
@@ -58,8 +64,39 @@ define dspace::apache_site ($hostname        = $name,
             'reverse_urls' => "ajp://localhost:${tomcat_ajp_port}/"
           }
         ],
+        docroot        => undef,
         manage_docroot => false,  # Don't create <Directory> or DocumentRoot
       }
+
+
+      # Check if SSL is enabled
+      if $ssl {
+
+        # Enable mod_ssl
+        class { 'apache::mod::ssl': }
+
+        # Create a corresponding SSL (HTTPS) virtual host on port 443
+        # and also proxy ALL requests to Tomcat's AJP port.
+        apache::vhost { "ssl-${hostname}":
+          servername => $hostname,
+          port       => 443,
+          ssl        => true,
+          # If $ssl_cert, etc are unspecified, use defaults from 'apache' module itself
+          ssl_cert   => $ssl_cert ? { undef => $apache::default_ssl_cert, default => $ssl_cert },
+          ssl_key    => $ssl_key ? { undef => $apache::default_ssl_key, default => $ssl_key },
+          ssl_chain  => $ssl_chain ? { undef => $apache::default_ssl_chain, default => $ssl_chain },
+          proxy_pass => [
+            {
+              'path'         => '/',
+              'url'          => "ajp://localhost:${tomcat_ajp_port}/",
+              'reverse_urls' => "ajp://localhost:${tomcat_ajp_port}/"
+            }
+          ],
+          docroot        => undef,
+          manage_docroot => false,  # Don't create <Directory> or DocumentRoot
+        }
+      }
+
     }
 
     # Absent = Uninstall Apache
